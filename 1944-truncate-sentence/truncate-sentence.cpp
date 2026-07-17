1ef2d198393849f6027ee48a1ec8d5e27f3830d0
priyankamalleshgoudapatil <priyankamallesh07@gmail.com>
2026-07-17 10:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (42.20%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        int sc = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]==' ') sc++;
+            if(sc == k) return ans;
+            ans += s[i];
+        }
+        return ans;
+    }
+};
