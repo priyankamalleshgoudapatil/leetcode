b3a5893bb947f7e19bf7ec70b77da4accbcc6629
priyankamalleshgoudapatil <priyankamallesh07@gmail.com>
2026-07-29 14:44:26 +0530

    Time: 3 ms (3.77%) | Memory: 8.6 MB (31.53%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(auto ch : s){
+            if(ch == '('){
+                st.push(ch);
+            }else if(!st.empty() && st.top() == '('){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
+};
